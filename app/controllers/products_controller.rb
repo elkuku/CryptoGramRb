553ba330fa12c7c0fb5ef5dab8ec731ca62b742f
elkuku <der.el.kuku@gmail.com>
2025-02-14 19:50:13 -0500

    i18n

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+  allow_unauthenticated_access only: %i[ index show ]
   before_action :set_product, only: %i[ show edit update destroy ]
 
   def index
@@ -45,6 +46,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.expect(product: [:name])
+    params.expect(product: [:name, :description, :featured_image])
   end
 end
